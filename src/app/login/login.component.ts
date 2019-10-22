f64d1f80f976e583d0a09cdc7492f5c660c73894
christopher <tomiwa_chris@yahoo.com>
2019-10-22 23:40:20 +0100

    connected the login page to the backend login API

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { Scrumuser, loginuser } from "../scrumuser";
+import { ScrumdataService } from "../scrumdata.service";
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  scrumUserLoginData = {}
 
-  constructor() { }
+  constructor(private _scrumdataService: ScrumdataService){}
 
   ngOnInit() {
   }
   scrumloginModel = new loginuser ( "", "", "");
+
+  onLoginSubmit(){
+    this._scrumdataService.login(this.scrumUserLoginData).subscribe(
+      data => console.log("Success", data),
+      error => console.log("Error", error)
+    )
+  }
 }
--- src/app/scrumdata.service.ts
+++ src/app/scrumdata.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Scrumuser } from './scrumuser';
 
+
 @Injectable({
   providedIn: 'root'
 })
@@ -9,7 +10,8 @@ export class ScrumdataService {
 
   constructor(private _http: HttpClient) {}
 
-  _url= "https://liveapi.chatscrum.com/scrum/api/scrumusers/"
+  _url= "https://liveapi.chatscrum.com/scrum/api/scrumusers/";
+  _loginUrl = "https://liveapi.chatscrum.com/scrum/api-token-auth/";
 
   public httpOptions = {
         headers: new HttpHeaders({"Content-Type": "application/json"})
@@ -17,5 +19,8 @@ export class ScrumdataService {
 
   signup(user: Scrumuser){
     return this._http.post<any>(this._url,{"email": user["email"], "password": user["password"], "full_name": user["fullname"], "usertype": user["type"],}, this.httpOptions );
+  }
+  login(user){
+    return this._http.post<any>(this._loginUrl,{"username": user["email"], "password": user["password"], "project": user["project"]}, this.httpOptions );
   }
 }
